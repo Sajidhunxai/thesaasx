7834a0cdfa0f157c9904bf93be05b48ea477744f
Qasim Fida <78118504+qasimfida@users.noreply.github.com>
2025-01-04 04:23:52 +0500

    changes

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Start and end dates
-start_date="2020-08-01 00:00:00"
-end_date="2024-08-05 00:00:00"
+start_date="2021-08-03 00:00:00"
+end_date="2021-08-05 00:00:00"
 
 # Define a list of random words for commit messages
 words=("refactor" "fix" "update" "optimize" "cleanup" "enhance" "test" "improve" "debug" "add" "remove" "merge")
@@ -12,7 +12,7 @@ start_date_epoch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$start_date" +"%s")
 end_date_epoch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$end_date" +"%s")
 
 # Set the total number of commits to make (adjustable)
-total_commits=300
+total_commits=10
 
 for i in $(seq 1 $total_commits); do
     # Generate a random timestamp between start_date and end_date
