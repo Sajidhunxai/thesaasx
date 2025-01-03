44f111f19ecfeac9076c14e2f13034fe12439dbc
Qasim Fida <78118504+qasimfida@users.noreply.github.com>
2025-01-04 04:13:24 +0500

    update

--- script.sh
+++ script.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-# Start date for August 2024
-start_date="2024-07-02 00:00:00"
-# Set the number of commits to make
-num_commits=3
-# Commit message (you can customize this if needed)
-commit_message="lawnpro"
-# Loop to create the commits
-for i in $(seq 1 $num_commits)
-do
-    # Modify a file for each commit, appending a new line with the commit number
-    echo "Commit #$i on $(date -d "$start_date + $i days" +"%Y-%m-%d %H:%M:%S")" >> commit_log.txt
+
+# Start and end dates
+start_date="2020-08-01 00:00:00"
+end_date="2024-08-05 00:00:00"
+
+# Define a list of random words for commit messages
+words=("refactor" "fix" "update" "optimize" "cleanup" "enhance" "test" "improve" "debug" "add" "remove" "merge")
+
+# Convert start_date and end_date to seconds since epoch
+start_date_epoch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$start_date" +"%s")
+end_date_epoch=$(date -j -f "%Y-%m-%d %H:%M:%S" "$end_date" +"%s")
+
+# Set the total number of commits to make (adjustable)
+total_commits=300
+
+for i in $(seq 1 $total_commits); do
+    # Generate a random timestamp between start_date and end_date
+    random_date_epoch=$((start_date_epoch + RANDOM % (end_date_epoch - start_date_epoch + 1)))
+    random_date=$(date -j -f "%s" "$random_date_epoch" +"%Y-%m-%d %H:%M:%S")
+
+    # Select a random word for the commit message
+    random_word=${words[$RANDOM % ${#words[@]}]}
+
+    # Modify a file for each commit
+    echo "Random Commit #$i on $random_date" >> commit_log.txt
     git add commit_log.txt
-    # Set the custom commit date for each commit
-    commit_date=$(date -d "$start_date + $i days" +"%Y-%m-%d %H:%M:%S")
-    # Commit with the custom date
-    GIT_AUTHOR_DATE="$commit_date" GIT_COMMITTER_DATE="$commit_date" git commit --no-edit --date "$commit_date" -m "$commit_message"
+
+    # Commit with the random date and message
+    GIT_AUTHOR_DATE="$random_date" GIT_COMMITTER_DATE="$random_date" git commit --no-edit --date "$random_date" -m "$random_word"
 done
+
 # Push all commits to the repository at once
 git push origin main --force
